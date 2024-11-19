214f56f739b9210489834dbff1428b6d023aff3a
Martial Bret <martial.bret@fms-ea.com>
2024-11-19 13:21:49 +0100

    Fin corrections

--- voisins_malins/app/urls.py
+++ voisins_malins/app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Affiche la page d'accueil
-    path('signup/', views.signup, name='signup'),  # Page d'inscription
+    path('signup/', views.signup_view, name='signup'),  # Page d'inscription
     path('login/', views.login_view, name='login'),  # Page de connexion
 ]
