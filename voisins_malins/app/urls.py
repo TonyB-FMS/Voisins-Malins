d68e00fd52154cc476c91027729207cb6694022d
Martial Bret <martial.bret@fms-ea.com>
2024-11-19 13:17:44 +0100

    Corrections des méthodes de vues

--- voisins_malins/app/urls.py
+++ voisins_malins/app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),  # Affiche la page d'accueil
     path('signup/', views.signup, name='signup'),  # Page d'inscription
-    path('login/', views.login, name='login'),  # Page de connexion
+    path('login/', views.login_view, name='login'),  # Page de connexion
 ]
