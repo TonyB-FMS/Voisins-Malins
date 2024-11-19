fc6623f9f4d95dd529693386d2751e3dbe83460e
Martial Bret <martial.bret@fms-ea.com>
2024-11-19 13:44:50 +0100

    Ajout méthode __str__ à la classe AuthentocatedUser

--- voisins_malins/app/models.py
+++ voisins_malins/app/models.py
@@ -47,6 +47,9 @@ class AuthenticatedUser(Visitor):
     password = models.CharField(max_length=128)
     skills = models.ManyToManyField('Skill', blank=True)
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
     def add_skill(self, skill):
         """
         Ajoute une compétence à l'utilisateur.
